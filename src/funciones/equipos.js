6962435e7f843df73f8c9ff0061f6c6e6022da2c
Sergio Paco Gómez <154501900+SergioPG92@users.noreply.github.com>
2025-06-08 11:18:35 +0200

    Update equipos.js

--- src/funciones/equipos.js
+++ src/funciones/equipos.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL+'/api';
+const API_URL = import.meta.env.VITE_API_URL+'api';
 
 // Función para crear un nuevo equipo, recibiendo el id del entrenador para asociarlo a él.
 const crearEquipo = async (nombre, entrenador) => {
