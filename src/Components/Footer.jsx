96efd96fba0136e90fe161b429c610f996378538
Sergio Paco Gómez <154501900+SergioPG92@users.noreply.github.com>
2025-06-09 09:28:05 +0200

    Update Footer.jsx
    Añadido el separador entre partes del footer.
    Ahora las partes se colocan en columna en pantallas menores a md.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,20 +3,23 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-dark-verde text-white py-2  font-fuente2">
-      <div className="container mx-auto px-4 flex justify-between items-center text-center gap-4">
+      <div className="mx-auto px-4 flex justify-between items-center text-center gap-4 flex-col md:flex-row">
         {/* Parte izquierda del footer */}
-        <div className="flex flex-col items-center justify-center">
-          <p className="text-m">
-            © 2025 Soccer Manager  </p>Este trabajo está bajo una licencia
-            <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.es">Creative
-            Commons Atribución-CompartirIgual 4.0 Internacional (CC BY-SA 4.0)</a>
+        <div className="flex flex-col items-center justify-center order-3 md:order-1">
+          <p className="text-m">© 2025 Soccer Manager </p>Este trabajo está bajo
+          una licencia
           <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.es">
-          <img src="/imagenes/CC_BY-SA_logo.svg" alt="" /></a>
+            Creative Commons Atribución-CompartirIgual 4.0 Internacional (CC
+            BY-SA 4.0)
+          </a>
+          <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.es">
+            <img src="/imagenes/CC_BY-SA_logo.svg" alt="imagen derechos" />
+          </a>
           <p className="text-m">Desarrollado por Sergio Paco Gómez</p>
         </div>
-
+        <hr class="solid block md:hidden order-2"></hr>
         {/* Parte derecha del footer */}
-        <div className="flex flex-col justify-center text-left  ">
+        <div className="flex flex-col justify-center text-left order-1 md:order-3">
           <div>
             <p className="text-m">
               <strong>Contacto</strong>
